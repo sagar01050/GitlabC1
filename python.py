db283e0998e4cbdd7a657988f4cb1cf4a19266c0
sagar01050 <sagarmanjupatil@gmail.com>
2024-11-14 14:40:20 +0530

    Create python.py

---
+++ python.py
@@ -0,0 +1,4 @@
+name=(int("Enter the name of the student"))
+usn=(int("Enter the usn of the student"))
+m1=(int("enter the marks of the 1st subject"))
+m2=(int("enter the marks of the 2nd subject:))
