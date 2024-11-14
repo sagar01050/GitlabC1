fb86c3e1c4c0d79ece5545e5bc4ae30f52897711
sagar01050 <sagarmanjupatil@gmail.com>
2024-11-14 15:25:07 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,4 +1,9 @@
-name=(int("Enter the name of the student"))
-usn=(int("Enter the usn of the student"))
-m1=(int("enter the marks of the 1st subject"))
-m2=(int("enter the marks of the 2nd subject:))
+name=input("Enter the name of the student"))
+usn=int(input("Enter the usn of the student"))
+m1=int(input("enter the marks of the 1st subject"))
+m2=int(input("enter the marks of the 2nd subject:"))
+m3=int(input("enter the marks of the 3rd subject:"))
+total=m1+m2+m3
+per=total/300*100
+print("name is",name)
+print("usn is",usn)
